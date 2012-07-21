d3040106de8ea43b8b1b13908e84eafd05018c2e
adamjacobbecker <ad@mbecker.cc>
2012-07-21 14:03:35 -0700

    allow CORS?

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,6 +1,28 @@
-  class StationsController < ApplicationController
-  # GET /stations
-  # GET /stations.json
+class StationsController < ApplicationController
+
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
   def index
     if params[:lat] && params[:lng]
       @stations = Station.near([params[:lat], params[:lng]], 200)
