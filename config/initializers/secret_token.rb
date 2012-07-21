6c7ca73c3a344116ba8a204c5b4b390a503c7199
adamjacobbecker <ad@mbecker.cc>
2012-07-21 12:09:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OaklandPumpsItUp::Application.config.secret_token = '903e0a54550307d926a95ef0c3617d96e28127809df79a14bfec06cb70d4d64cb43341df4776ffad9f2a0efb46734ca37e623cdd571e86cc632a128f588e7639'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OaklandPumpsItUp::Application.config.session_store :cookie_store, key: '_OaklandPumpsItUp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OaklandPumpsItUp::Application.config.session_store :active_record_store
